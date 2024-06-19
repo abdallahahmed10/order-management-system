a4a3bec385c4fb0d8823e350d638c8b8fc0c413e
abdallahahmed10 <abdallah.ahmed581@gmail.com>
2024-06-19 20:01:24 +0300

    Added createOrder in order service

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import { Order, OrderItem } from '@prisma/client';
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+    constructor(private prisma: PrismaService) {}
+
+    async createOrder(userId: number): Promise<Order> {
+        const cart = await this.prisma.cart.findUnique({
+          where: { userId },
+          include: { cartItems: true }
+        });
+    
+        if (!cart || !cart.cartItems.length) throw new Error('Cart is empty');
+    
+        const order = await this.prisma.order.create({
+          data: {
+            userId,
+            status: 'Pending',
+            orderItems: {
+              create: cart.cartItems.map(item => ({
+                productId: item.productId,
+                quantity: item.quantity
+              }))
+            }
+          }
+        });
+    
+        await this.prisma.cartItem.deleteMany({ where: { cartId: cart.cartId } });
+    
+        return order;
+      }
+
+
+}
