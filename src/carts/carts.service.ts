e14a7b7b063523600cd52a1eb91911ff12947e16
abdallahahmed10 <abdallah.ahmed581@gmail.com>
2024-06-19 19:39:16 +0300

    Added addToCart function in cart service

--- src/carts/carts.service.ts
+++ src/carts/carts.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import { Cart, CartItem } from '@prisma/client';
 
 @Injectable()
-export class CartsService {}
+export class CartsService {
+    constructor(private prisma: PrismaService) {}
+
+    async addToCart(userId: number, productId: number, quantity: number): Promise<CartItem> {
+        // Find the cart by userId
+        const cart = await this.prisma.cart.upsert({
+          where: { userId },
+          update: {},
+          create: { userId }
+        });
+    
+        // Upsert the cart item
+        return this.prisma.cartItem.upsert({
+          where: {
+            cartId_productId: {
+              cartId: cart.cartId,
+              productId
+            }
+          },
+          update: { quantity: { increment: quantity } },
+          create: { cartId: cart.cartId, productId, quantity }
+        });
+      }
+}
