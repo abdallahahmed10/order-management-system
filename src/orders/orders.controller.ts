c1441947486c468b7eb27674c0b8b8fe7f7eb536
abdallahahmed10 <abdallah.ahmed581@gmail.com>
2024-06-19 20:40:06 +0300

    Added API's for orders

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Put, Body, Param } from '@nestjs/common';
+import { OrdersService } from './orders.service';
 
-@Controller('orders')
-export class OrdersController {}
+@Controller('api/orders')
+export class OrdersController {
+    constructor(private ordersService: OrdersService) {}
+
+    @Post()
+  createOrder(@Body() body: { userId: number }) {
+    return this.ordersService.createOrder(body.userId);
+  }
+
+  @Get(':orderId')
+  getOrderById(@Param('orderId') orderId: number) {
+    return this.ordersService.getOrderById(orderId);
+  }
+
+  @Put(':orderId/status')
+  updateOrderStatus(@Param('orderId') orderId: number, @Body() body: { status: string }) {
+    return this.ordersService.updateOrderStatus(orderId, body.status);
+  }
+
+  @Get('user/:userId')
+  getOrdersByUserId(@Param('userId') userId: number) {
+    return this.ordersService.getOrdersByUserId(userId);
+  }
+  
+}
