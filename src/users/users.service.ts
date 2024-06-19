1b677efe0f64f4018e1ac64914c5fbc0b4d69a1b
abdallahahmed10 <abdallah.ahmed581@gmail.com>
2024-06-19 19:38:12 +0300

    Modified users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import { User } from '@prisma/client';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private prisma: PrismaService) {}
+    
+    async createUser(data: User): Promise<User> {
+        return this.prisma.user.create({ data });
+    }
+    
+    async getUserById(userId: number): Promise<User> {
+        return this.prisma.user.findUnique({ where: { userId } });
+    }
+    
+    async getUsers() {
+        return this.prisma.user.findMany();
+    }
+    
+    async updateUser(userId: number, data: User) {
+        return this.prisma.user.update({ where: { userId }, data });
+    }
+    
+    async deleteUser(userId: number) {
+        return this.prisma.user.delete({ where: { userId } });
+    }
+}
