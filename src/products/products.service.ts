284e20c6357f38f2013373702412734931cbfbb0
abdallahahmed10 <abdallah.ahmed581@gmail.com>
2024-06-19 19:38:41 +0300

    Modified products service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import { Product } from '@prisma/client';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    constructor(private prisma: PrismaService) {}
+
+    async createProduct(data: any): Promise<Product> {
+        return this.prisma.product.create({ data });
+    }
+
+    async getProductById(productId: number): Promise<Product> {
+        return this.prisma.product.findUnique({ where: { productId } });
+    }
+
+    async getProducts() {
+        return this.prisma.product.findMany();
+    }
+
+    async updateProduct(productId: number, data: any) {
+        return this.prisma.product.update({ where: { productId }, data });
+    }
+
+    async deleteProduct(productId: number) {
+        return this.prisma.product.delete({ where: { productId } });
+    }
+}
