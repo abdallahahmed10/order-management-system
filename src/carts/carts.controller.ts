6f818968c84c6d2cdd73f33f87b91f27a0fae346
abdallahahmed10 <abdallah.ahmed581@gmail.com>
2024-06-19 20:39:41 +0300

    Added CRUD for cart

--- src/carts/carts.controller.ts
+++ src/carts/carts.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Put, Delete, Body, Param } from '@nestjs/common';
+import { CartsService } from './carts.service';
 
-@Controller('carts')
-export class CartsController {}
+@Controller('api/carts')
+export class CartsController {
+    constructor(private cartsService: CartsService) {}
+
+    @Post('add')
+    addToCart(@Body() data: { userId: number, productId: number, quantity: number }) {
+        return this.cartsService.addToCart(data.userId, data.productId, data.quantity);
+    }
+
+    @Get(':userId')
+    getCartByUserId(@Param('userId') userId: number) {
+        return this.cartsService.getCartByUserId(userId);
+    }
+
+    @Put('update')
+    updateCart(@Body() data: { userId: number, productId: number, quantity: number }) {
+        return this.cartsService.updateCart(data.userId, data.productId, data.quantity);
+    }
+
+    @Delete('remove')
+    removeFromCart(@Body() data: { userId: number, productId: number }) {
+        return this.cartsService.removeFromCart(data.userId, data.productId);
+    }
+}
