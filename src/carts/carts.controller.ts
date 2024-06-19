a53c29425025a221be090f2a1c0ea64ee14c7779
abdallahahmed10 <abdallah.ahmed581@gmail.com>
2024-06-19 21:18:30 +0300

    Modified carts and orders params in controller

--- src/carts/carts.controller.ts
+++ src/carts/carts.controller.ts
@@ -1,26 +1,26 @@
 import { Controller, Post, Get, Put, Delete, Body, Param } from '@nestjs/common';
 import { CartsService } from './carts.service';
 
-@Controller('api/carts')
+@Controller('api/cart')
 export class CartsController {
     constructor(private cartsService: CartsService) {}
 
-    @Post('add')
+    @Post('/add')
     addToCart(@Body() data: { userId: number, productId: number, quantity: number }) {
         return this.cartsService.addToCart(data.userId, data.productId, data.quantity);
     }
 
-    @Get(':userId')
-    getCartByUserId(@Param('userId') userId: number) {
-        return this.cartsService.getCartByUserId(userId);
+    @Get('/:userId')
+    getCartByUserId(@Param('userId') userId: string) {
+        return this.cartsService.getCartByUserId(+userId);
     }
 
-    @Put('update')
+    @Put('/update')
     updateCart(@Body() data: { userId: number, productId: number, quantity: number }) {
         return this.cartsService.updateCart(data.userId, data.productId, data.quantity);
     }
 
-    @Delete('remove')
+    @Delete('/remove')
     removeFromCart(@Body() data: { userId: number, productId: number }) {
         return this.cartsService.removeFromCart(data.userId, data.productId);
     }
--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -11,18 +11,18 @@ export class OrdersController {
   }
 
   @Get(':orderId')
-  getOrderById(@Param('orderId') orderId: number) {
-    return this.ordersService.getOrderById(orderId);
+  getOrderById(@Param('orderId') orderId: string) {
+    return this.ordersService.getOrderById(+orderId);
   }
 
   @Put(':orderId/status')
-  updateOrderStatus(@Param('orderId') orderId: number, @Body() body: { status: string }) {
-    return this.ordersService.updateOrderStatus(orderId, body.status);
+  updateOrderStatus(@Param('orderId') orderId: string, @Body() body: { status: string }) {
+    return this.ordersService.updateOrderStatus(+orderId, body.status);
   }
 
   @Get('user/:userId')
-  getOrdersByUserId(@Param('userId') userId: number) {
-    return this.ordersService.getOrdersByUserId(userId);
+  getOrdersByUserId(@Param('userId') userId: string) {
+    return this.ordersService.getOrdersByUserId(+userId);
   }
   
 }
